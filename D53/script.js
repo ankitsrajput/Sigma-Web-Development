aa6f8431536880a0c5c5334ba26ff5f997156057
Ankit Singh <ankitsrajputmail@gmail.com>
2025-09-22 23:06:24 +0530

    Add files via upload

---
+++ D53/script.js
@@ -0,0 +1,15 @@
+alert("WelCome To Js Journey..!!");
+
+console.log("Your Code is Running.");
+console.log("Your Code is Failed Some Reason.");
+
+var a = prompt("Enter You Number : ");
+console.log("You Number is : " + a);
+
+var isTrue = confirm("Are Your Sure To Leave This Page ?");
+if(isTrue){
+    console.log("Computer is Hanged.");
+}
+else{
+    console.log("Computer is Running Smmothly.");
+}
