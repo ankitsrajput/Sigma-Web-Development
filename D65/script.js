5c5bd1af76fd5345a7545231582cb4795877fd89
Ankit Singh <ankitsrajputmail@gmail.com>
2025-12-30 11:52:03 +0530

    Add files via upload

---
+++ D65/script.js
@@ -0,0 +1,6 @@
+// DOM: Document Object Model
+// BOM: Browser Object Model
+
+document.title = "DOM : Document Object Model."; //changed title of webpage
+console.log(document.body);
+document.body.style.backgroundColor = "cyan";
